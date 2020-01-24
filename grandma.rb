209287bf280a06b37652c17e891948b8712b689e
Stacy Johns <stacykjohns@icloud.com>
2020-01-24 02:58:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-   elsif phrase.upcase
-     "NO, NOT SINCE 1938!"
-   else 
+   elsif phrase != phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
+   else phrase.upcase
+     "NO, NOT SINCE 1938!"
   end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
