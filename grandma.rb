4901900536aa5d205e767f529ca4dc9fdeaf0497
Stacy Johns <stacykjohns@icloud.com>
2020-01-24 02:56:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-   elsif phrase = !phrase.upcase
+   elsif phrase == !phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
    else phrase.upcase
      "NO, NOT SINCE 1938!"
